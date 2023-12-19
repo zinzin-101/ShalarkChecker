4fa8f4b98ee1c9f5838667fa492f00ead8dd3134
zinzin-101 <mr.punthep@gmail.com>
2023-12-20 01:14:07 +0700

    mvp

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from pyscript import document
 import json
 
+data = {
+    "result":
+    [
+        "921688",
+        "903444",
+        ["816183","902479","876088","979810"],
+        ["928285","992154"],
+        ["836094","889499","860003","900376","860946","911575","863811","980565","872941","995690"]
+    ]
+}
+
 results = []
 
 def test(event):
@@ -9,14 +20,31 @@ def test(event):
 
     x.innerText = y.value
 
-def getNumber():
-    file = open("result.json", "r")
-    data = json.load(file)
-
+def get_number():
     for info in data["result"]:
         results.append(info)
 
-    file.close()
+def check_user_input(event):
+    user_input = document.querySelector("#num-input")
+    result_text = document.querySelector("#result")
 
-getNumber()
-print(results)
+    _input = user_input.value
+
+    if len(_input) != 6:
+        result_text.innerText = "Invalid Input"
+        return
+
+    for i in range(5):
+        if len(results[i]) == 1:
+            if _input == results[i]:
+                result_text.innerText = f"ถูกรางวัลที่ {i+1}"
+                return
+        
+        for j in results[i]:
+            if _input == j:
+                result_text.innerText = f"ถูกรางวัลที่ {i+1}"
+                return
+    result_text.innerText = "ไม่ถูกรางวัล"
+
+get_number()
+
